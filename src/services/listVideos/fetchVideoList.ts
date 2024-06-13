c85ffa557138a2c76e122c93cafe5f7b25eb3fef
David <ponchitosexi@hotmail.com>
2024-06-13 03:36:43 -0700

    feat: Apikey change

--- src/services/listVideos/fetchVideoList.ts
+++ src/services/listVideos/fetchVideoList.ts
@@ -1,8 +1,11 @@
 import { instance } from '@/services/instance';
 import { videoListSchema } from '@/types/schemas/videoList';
 import { youtubeInstance } from '../youtubeInstance';
+import { string } from 'zod';
+
+export const apiKey: string = 'AIzaSyDrJ6V44t-TTSbHfdTq7rCYSWI5BSQOmTI'
 
 export default async (search: string) => {
-	const response = await youtubeInstance.get(`search?part=snippet&q=${search}h&maxResults=15&type=video&key=AIzaSyDrJ6V44t-TTSbHfdTq7rCYSWI5BSQOmTI`).json();
+	const response = await youtubeInstance.get(`search?part=snippet&q=${search}h&maxResults=15&type=video&key=${apiKey}`).json();
 	return videoListSchema.parse(response);
 };
