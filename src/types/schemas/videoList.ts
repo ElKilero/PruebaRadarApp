4b6542bbcf1aba4e66ade91a917de7f32b520d6c
David <ponchitosexi@hotmail.com>
2024-06-13 02:06:28 -0700

    feat: schema thumbnail correction

--- src/types/schemas/videoList.ts
+++ src/types/schemas/videoList.ts
@@ -1,76 +1,77 @@
 import { z } from "zod";
 
 export const videoListSchema = z.object({
-    kind: z.string(),
-    etag: z.string(),
-    nextPageToken: z.string(),
-    regionCode: z.string(),
-    pageInfo: z.object({ totalResults: z.number(), resultsPerPage: z.number() }),
-    items: z.array(
-      z.union([
-        z.object({
-          kind: z.string(),
-          etag: z.string(),
-          id: z.object({ kind: z.string(), videoId: z.string() }),
-          snippet: z.object({
-            publishedAt: z.string(),
-            channelId: z.string(),
-            title: z.string(),
-            description: z.string(),
-            thumbnails: z.object({
-              default: z.object({
-                url: z.string(),
-                width: z.number(),
-                height: z.number()
-              }),
-              medium: z.object({
-                url: z.string(),
-                width: z.number(),
-                height: z.number()
-              }),
-              high: z.object({
-                url: z.string(),
-                width: z.number(),
-                height: z.number()
-              })
-            }),
-            channelTitle: z.string(),
-            liveBroadcastContent: z.string(),
-            publishTime: z.string()
+  kind: z.string(),
+  etag: z.string(),
+  nextPageToken: z.string(),
+  regionCode: z.string(),
+  pageInfo: z.object({ totalResults: z.number(), resultsPerPage: z.number() }),
+  items: z.array(
+    z.object({
+      kind: z.string(),
+      etag: z.string(),
+      id: z.object({ kind: z.string(), videoId: z.string() }),
+      snippet: z.object({
+        publishedAt: z.string(),
+        channelId: z.string(),
+        title: z.string(),
+        description: z.string(),
+        thumbnails: z.object({
+          default: z.object({
+            url: z.string(),
+            width: z.number(),
+            height: z.number()
+          }),
+          medium: z.object({
+            url: z.string(),
+            width: z.number(),
+            height: z.number()
+          }),
+          high: z.object({
+            url: z.string(),
+            width: z.number(),
+            height: z.number()
           })
         }),
-        z.object({
-          kind: z.string(),
-          etag: z.string(),
-          id: z.object({ kind: z.string(), playlistId: z.string() }),
-          snippet: z.object({
-            publishedAt: z.string(),
-            channelId: z.string(),
-            title: z.string(),
-            description: z.string(),
-            thumbnails: z.object({
-              default: z.object({
-                url: z.string(),
-                width: z.number(),
-                height: z.number()
-              }),
-              medium: z.object({
-                url: z.string(),
-                width: z.number(),
-                height: z.number()
-              }),
-              high: z.object({
-                url: z.string(),
-                width: z.number(),
-                height: z.number()
-              })
-            }),
-            channelTitle: z.string(),
-            liveBroadcastContent: z.string(),
-            publishTime: z.string()
-          })
-        })
-      ])
-    )
-  })
-  
+        channelTitle: z.string(),
+        liveBroadcastContent: z.string(),
+        publishTime: z.string()
+      })
+    })
+  )
+})
+
+export interface VideoListItemProp {
+  kind: string;
+  etag: string;
+  id: id;
+  snippet: snippet;
+}
+
+interface id {
+  kind: string;
+  videoId: string;
+}
+
+interface thumbnails {
+  default: thumbnailDetail;
+  medium: thumbnailDetail;
+  high: thumbnailDetail;
+}
+
+interface thumbnailDetail {
+  url: string;
+  width: number;
+  height: number;
+}
+
+interface snippet {
+  publishedAt: string;
+  channelId: string;
+  title: string;
+  description: string;
+  thumbnails: thumbnails;
+  channelTitle: string;
+  liveBroadcastContent: string;
+  publishTime: string;
+}
